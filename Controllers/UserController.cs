34dbfca6181ce543198df098714fb4b54536d390
simaosousa10 <simaosousa@ipvc.pt>
2025-03-17 20:06:32 +0000

    Test controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿
+
+using ESIID42025.Data;
+using ESIID42025.Models;
+
+namespace ESIID42025.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -8,7 +8,7 @@ public class User
     public int Id { get; set; }
     
     public string Username { get; set; }
-    public string Email { get; set; }
+    public string Email { get; set; } 
     public string Name { get; set; }
     public string Password { get; set; }
     public DateTime RegisterDate { get; set; }
