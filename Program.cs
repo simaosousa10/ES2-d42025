9640145e5b8bc772e4ee66b9c79e5d8053cc4c46
simaosousa10 <simaosousa@ipvc.pt>
2025-03-17 18:26:59 +0000

    Prep Code DB

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using ESIID42025.Components;
+using ESIID42025.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
