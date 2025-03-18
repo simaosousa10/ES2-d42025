13614e5df44af418c2360a8d2bb3f91079b797b7
simaosousa10 <simaosousa@ipvc.pt>
2025-03-18 13:33:01 +0000

    Create User Authentication and Authorization code

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace ESIID42025.Models;
 
-public class User
+public class User : IdentityUser<int>
 {
-    [Key]
-    public int Id { get; set; }
-    
-    public string Username { get; set; }
-    public string Email { get; set; } 
     public string Name { get; set; }
-    public string Password { get; set; }
     public DateTime RegisterDate { get; set; }
     
     // Navigation Properties
